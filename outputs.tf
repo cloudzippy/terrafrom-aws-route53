ba6f00aab2f21ec639d67a980905485a0cc2fe41
Karthikeyan S <80709791+cloudzippy@users.noreply.github.com>
2024-11-19 13:28:20 +0530

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -1,9 +1,1 @@
-output "hosted_zone_id" {
-  description = "The ID of the hosted zone"
-  value       = aws_route53_zone.this[0].id
-}
 
-output "name_servers" {
-  description = "The name servers of the hosted zone"
-  value       = aws_route53_zone.this[0].name_servers
-}
