64eea5ab8abdf9c9510ac83af7f099aab2afd61b
Karthikeyan S <80709791+cloudzippy@users.noreply.github.com>
2024-11-19 12:55:14 +0530

    Create variables.tf

---
+++ variables.tf
@@ -0,0 +1,22 @@
+variable "create_private_zone" {
+  description = "Toggle to create a private hosted zone. If false, creates a public zone."
+  type        = bool
+  default     = false
+}
+
+variable "domain_name" {
+  description = "The domain name for the hosted zone"
+  type        = string
+}
+
+variable "vpc_id" {
+  description = "The ID of the VPC for a private hosted zone (only used if private)"
+  type        = list(string)
+  default     = ""
+}
+
+variable "tags" {
+  description = "Tags to apply to the hosted zone"
+  type        = map(string)
+  default     = {}
+}
